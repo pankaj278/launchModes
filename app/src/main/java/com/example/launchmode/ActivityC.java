1eddd3a2d6b51a7f38af6b391d376f16c3ff04a2
Pankaj <pankajsharma270895@gmail.com>
2018-09-13 18:52:09 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/launchmode/ActivityC.java
@@ -0,0 +1,62 @@
+package com.example.launchmode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class ActivityC extends AppCompatActivity {
+
+    private static final String TAG = "AActivityD";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c);
+        Log.d(TAG,"onCreate");
+        findViewById(R.id.cBTN).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityC.this,ActivityD.class);
+                intent.putExtra("DATA","VALUE");
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+}
